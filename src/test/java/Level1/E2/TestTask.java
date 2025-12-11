b4188b8a8b789eaed592ae33fd3b8c93cfa73544
cristhianchulca49 <cristhianchulca49@gmail.com>
2025-12-11 12:07:12 +0100

    test: add testTask to evaluate checkBattery() for all Level enum values

---
+++ src/test/java/Level1/E2/TestTask.java
@@ -0,0 +1,14 @@
+package Level1.E2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestTask {
+    @Test
+    public void testCheckBattery() {
+        assertEquals("LOW BATTERY: cannot star the car", Task.checkBattery(Level.LOW));
+        assertEquals("MEDIUM BATTERY: starting eco mode", Task.checkBattery(Level.MEDIUM));
+        assertEquals("HIGH BATTERY: starting car", Task.checkBattery(Level.HIGH));
+    }
+}
