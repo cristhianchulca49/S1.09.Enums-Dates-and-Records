e1f786d147300a14ace25456392f1e207d28b3cb
cristhianchulca49 <cristhianchulca49@gmail.com>
2025-12-11 13:22:32 +0100

    refactor: rewrite the message

--- src/main/java/Level1/E2_E3/Task.java
+++ src/main/java/Level1/E2_E3/Task.java
@@ -1,11 +1,11 @@
-package Level1.E2;
+package Level1.E2_E3;
 
 public class Task {
     public static String checkBattery(Level level) {
         return switch (level) {
             case LOW -> "LOW BATTERY: cannot star the car";
-            case MEDIUM -> "MEDIUM BATTERY: starting eco mode";
-            case HIGH -> "HIGH BATTERY: starting car";
+            case MEDIUM -> "MEDIUM BATTERY: can only star only eco mode";
+            case HIGH -> "HIGH BATTERY: can start in power mode";
         };
     }
 }
