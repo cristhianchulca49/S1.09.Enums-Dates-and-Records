f05f4e8a787b1e75a923a0983a666a1e7cc779f3
cristhianchulca49 <cristhianchulca49@gmail.com>
2025-12-11 12:27:43 +0100

    test: add System.out.println to print

--- src/main/java/Level1/E1/Main.java
+++ src/main/java/Level1/E1/Main.java
@@ -3,7 +3,7 @@ package Level1.E1;
 public class Main {
     public static void main(String[] args) {
         for (Day today : Day.values()) {
-            DayService.checkDay(today);
+            System.out.println(DayService.checkDay(today));
         }
     }
 }
