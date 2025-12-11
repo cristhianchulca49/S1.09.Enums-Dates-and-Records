f50093d8c42501a12c798b88ff40b9660747eb74
cristhianchulca49 <cristhianchulca49@gmail.com>
2025-12-11 12:13:39 +0100

    feat: replace if with switch expression to add new case

--- src/main/java/Level1/E2/Task.java
+++ src/main/java/Level1/E2/Task.java
@@ -2,6 +2,10 @@ package Level1.E2;
 
 public class Task {
     public static String checkBattery(Level level) {
-        return level.battery() ? level + " BATTERY: cannot star the car" : level + " BATTERY: starting car";
+        return switch (level) {
+            case LOW -> "LOW BATTERY: cannot star the car";
+            case MEDIUM -> "MEDIUM BATTERY: starting eco mode";
+            case HIGH -> "HIGH BATTERY: starting car";
+        };
     }
 }
