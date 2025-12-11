c7cb637a9074c339fcd2c71f703ade7c250c4d8b
cristhianchulca49 <cristhianchulca49@gmail.com>
2025-12-11 19:00:40 +0100

    test: add testCheckBattery() and testStartCar()

--- src/test/java/Level1/E3/TestCarServices.java
+++ src/test/java/Level1/E3/TestCarServices.java
@@ -1,5 +1,29 @@
 package Level1.E3;
 
-public class TestCarServices {
+import org.junit.Test;
 
+import static org.junit.Assert.*;
+
+public class TestCarServices {
+    @Test
+    public void testCheckBattery() {
+        int percentLow = 33;
+        int percentMedium = 50;
+        int percentHigh = 100;
+
+        assertEquals("LOW BATTERY: cannot start the car", CarServices.checkBattery(percentLow));
+        assertEquals("MEDIUM BATTERY: can only star only eco mode", CarServices.checkBattery(percentMedium));
+        assertEquals("HIGH BATTERY: can start in power mode", CarServices.checkBattery(percentHigh));
+    }
+
+    @Test
+    public void testStartCar() {
+        int percentLow = 33;
+        int percentMedium = 50;
+        int percentHigh = 100;
+
+        assertFalse(CarServices.startCar(percentLow));
+        assertTrue(CarServices.startCar(percentMedium));
+        assertTrue(CarServices.startCar(percentHigh));
+    }
 }
