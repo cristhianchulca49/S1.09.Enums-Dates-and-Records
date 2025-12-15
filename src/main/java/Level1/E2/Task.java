dccf95cbcdd18c280106056c0762f5db6a6d248e
cristhianchulca49 <cristhianchulca49@gmail.com>
2025-12-15 10:57:36 +0100

    fix: correct expected error message

--- src/main/java/Level1/E2/Task.java
+++ src/main/java/Level1/E2/Task.java
@@ -3,8 +3,8 @@ package Level1.E2;
 public class Task {
     public static String checkBattery(Level level) {
         return switch (level) {
-            case LOW -> "LOW BATTERY: cannot star the car";
-            case MEDIUM -> "MEDIUM BATTERY: can only star in eco mode";
+            case LOW -> "LOW BATTERY: cannot start the car";
+            case MEDIUM -> "MEDIUM BATTERY: can only start in eco mode";
             case HIGH -> "HIGH BATTERY: can start in power mode";
         };
     }
--- src/test/java/Level1/E2/TestTask.java
+++ src/test/java/Level1/E2/TestTask.java
@@ -2,13 +2,14 @@ package Level1.E2;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestTask {
     @Test
     public void testCheckBattery() {
-        assertEquals("LOW BATTERY: cannot star the car", Task.checkBattery(Level.LOW));
-        assertEquals("MEDIUM BATTERY: can only star only eco mode", Task.checkBattery(Level.MEDIUM));
+        assertEquals("LOW BATTERY: cannot start the car", Task.checkBattery(Level.LOW));
+        assertEquals("MEDIUM BATTERY: can only start in eco mode", Task.checkBattery(Level.MEDIUM));
         assertEquals("HIGH BATTERY: can start in power mode", Task.checkBattery(Level.HIGH));
     }
 }
